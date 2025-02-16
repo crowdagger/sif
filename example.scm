89afc57ea3a6bccf7f94afccdc20f5956c653f90
Lizzie Crowdagger <lizzie@crowdagger.fr>
2025-02-16 21:23:09 +0100

    Work on de/serialazing

--- example.scm
+++ example.scm
@@ -1,11 +1,33 @@
 (add-to-load-path ".")
+(add-to-load-path ".")
 
 (import (oop goops)
+        (sif world)
         (sif element))
 
-(define room (make <element> #:name "Room"))
-(define table (make <element> #:name "Table"))
+(define w (make <world>))
 
-(add room table)
+(define room (make <element> #:name "Room" #:world w))
+(define table (make <element> #:name "Table" #:world w))
+
+;(write table)
+(add! room table)
 
 (action room 'look-at)
+(newline)
+(write room)
+(newline)
+(write table)
+(newline)
+(define s (call-with-output-string
+            (lambda (port)
+              (write table port))))
+(display s)
+(newline)
+(define v (call-with-input-string s
+            (lambda (port)
+              (element-read w port))))
+
+(display v)
+(newline)
+
--- sif/element.scm
+++ sif/element.scm
@@ -1,14 +1,21 @@
 (define-module (sif element))
 (export <element>
-        list-actions action add)
+        element-read id list-actions action add!)
 
 (import (oop goops)
         (ice-9 optargs)
         (ice-9 match)
-        (srfi srfi-1))
+        (srfi srfi-1)
+        (sif world))
 
 ;;; Generic element, should be used by about anything
 (define-class <element> ()
+  (world #:init-value #f
+         #:init-keyword #:world
+         #:getter element-world)
+  (id #:init-value #f
+      #:init-keyword #:id
+      #:getter element-id)
   (name #:init-value ""
         #:getter name
         #:init-keyword #:name)
@@ -17,13 +24,59 @@
   (description #:init-value ""
                #:getter description
                #:init-keyword #:description)
-  (container #:init-value '()
-             #:getter container
+  (container #:init-value #f
+             #:getter element-container
              #:init-keyword #:container)
   (inner #:init-value '()
-         #:getter inner
+         #:getter element-inner
          #:init-keyword #:inner)
 )
+
+;;; Adds an element to another
+;;;
+;;; Elements must be in the same world (duh) else bad things will happen
+(define-generic add!)
+(define-method (add! (e1 <element>) (e2 <element>))
+  ;; First, e2 to e1
+  (let* ([world (element-world e1)]
+         [world2 (element-world e2)]
+         [id (element-id e2)]
+         [new-inner (cons id (element-inner e1))])
+    (unless (eq? world world2)
+      (error "Elements are from different worlds" e1 e2))
+    (slot-set! e1 'inner new-inner)
+    ;; Then, set e1 as the container of e2
+    (let ([previous (element-container e2)])
+      (slot-set! e2 'container
+                 (element-id e1))
+      ;; We also need to remove e2 from its previous container
+      (when previous
+        (let ([previous (world-ref world previous)])
+          (remove! previous e2))))))
+
+;;; Remove an element from another containing it
+(define-generic remove!)
+(define-method (remove! (e1 <element>) (e2 <element>))
+  (let* ([world (element-world e1)]
+         [world2 (element-world e2)]
+         [id (element-id e2)]
+         [new-list (delete id
+                           (element-inner e1))])
+    (unless (eq? world world2)
+      (error "Elements are from different worlds" e1 e2))
+    (slot-set! e1 'inner new-list)))
+  
+(define-method (initialize (e <element>) initargs)
+  (next-method)
+  ;; Add self to world and set id
+  (if (element-world e)
+      (slot-set! e 'id
+                 (world-add! (element-world e)
+                             e)))
+      
+  ;; Add default actions 
+  (add-action e 'look-at (λ (e) (look-at e)))
+  (add-action e 'add (λ (e1 e2) (add! e1 e2))))
 
 ;;; List all valid actions for an element
 (define-generic list-actions)
@@ -41,11 +94,30 @@
       (error "Unsupported action" action))
     (apply handler (cons e args))))
 
-(define-method (initialize (e <element>) initargs)
-  (next-method)
-  ;; Add default actions 
-  (add-action e 'look-at (λ (e) (look-at e)))
-  (add-action e 'add (λ (e1 e2) (add e1 e2))))
+
+(define (element-read world port)
+  "Read an element from optional port"
+  (match-let* ([(clss tail) (read port)])
+    (let ([e (make <element> #:world world)])
+      (for-each (lambda (v)
+                  (slot-set! e (car v) (cdr v)))
+                tail)
+      e)))
+
+(define-method (write (x <element>) . rest)
+  (let* ([ctnr (if (null? (element-container x))
+                   ""
+                   (format #f "(container . ~a)" (element-container x)))]
+         [s (format #f "(~a ((name . ~s) (description . ~s) (inner . ~a) ~a))"
+                   (class-name (class-of x))
+                   (name x)
+                   (description x)
+                   (if (null? (element-inner x))
+                       "()"
+                       (format #f "~s" (element-inner x)))
+                   ctnr
+                   )])
+  (apply display (cons s rest))))
 
 ;;; Adds an action to an element.
 ;;; Hander must be of the form (lambda (element . args))
@@ -59,8 +131,8 @@
 
 (define-generic message-up)
 (define-method (message-up (x <element>) msg)
-  (unless (nil? (container x))
-    (message-up (container x)
+  (unless (element-container x)
+    (message-up (element-container x)
                 msg)))
 
 (define-generic message-down)
@@ -71,21 +143,7 @@
   (display msg)
   (newline)
   (map (λ (x) (message-down x msg))
-       (inner x)))
-
-(define-generic add)
-(define-method (add (x <element>) (y <element>))
-  ;; Add y to x's inner
-  (slot-set! x
-             'inner
-             (cons y (inner x)))
-  (let ([previous (container y)])
-    (unless (nil? previous)
-      (slot-set! previous
-                 'inner
-                 (delete y (inner previous)))))
-  ;; Set x as y's container
-  (slot-set! y 'container x))
+       (element-inner x)))
 
 ;;; Command to look at object and show description.
 (define-generic look-at)
@@ -95,10 +153,12 @@
                 (display (name x))
                 (newline)
                 (when (and (not nested)
-                           (not (null? (inner x))))
+                           (not (null? (element-inner x))))
                   (display "Contains:\n")
                   (map (λ (e)
-                         (display (look-at e #:nested #t)))
-                       (inner x)))))
+                         (display (look-at (world-ref (element-world x)
+                                                      e)
+                                           #:nested #t)))
+                       (element-inner x)))))
                            
   
