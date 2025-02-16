f65339988c3e59b9b12c733b06eeafe9746c1cc3
Lizzie Crowdagger <lizzie@crowdagger.fr>
2025-02-17 00:32:19 +0100

    Add tests

--- tests/world.scm
+++ tests/world.scm
@@ -1,6 +1,7 @@
 (import (srfi srfi-64)
         (oop goops)
-        (sif world))
+        (sif world)
+        (sif element))
 
 
 (test-begin "world")
@@ -11,5 +12,20 @@
   (test-equal 42 (world-ref w 0))
   (world-del! w 0)
   (test-equal #f (world-ref w 0))
+  )
+
+(test-group "de/serialization"
+  (define w (make <world>))
+  (define room (make <element> #:name "Room" #:world w))
+  (define table (make <element> #:name "Table" #:world w))
+  (add! room table)
+  
+  (define lst (world->list w))
+  (define world2 (list->world lst))
+  (test-equal "Room" (slot-ref (world-ref world2 0) 'name))
+  (test-equal "Table" (slot-ref (world-ref world2 1) 'name))
+
+  (define lst2 (world->list world2))
+  (test-equal lst lst2)
   )
 (test-end "world")
